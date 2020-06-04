f6275a184c6e2fe23934d43aab95ae333030facd
hieu-ng <hieutnguyen06@gmail.com>
2020-06-04 10:03:41 +1000

    Listen to DOM events

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
+// Rules and close event handlers
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));
