f56594c34ca67176d907df6d34b9888880b29531
hieu-ng <hieutnguyen06@gmail.com>
2020-06-04 10:10:18 +1000

    Create ball props

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const rules = document.getElementById('rules');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+// Create ball props
+const ball = {
+	x: canvas.width / 2,
+	y: canvas.height / 2,
+	size: 10,
+	// speed: 4, 
+	// dx:4, 
+	// dy: -4,
+
+}
+
 // Keyboard event handlers
 document.addEventListener('keydown', keyDown);
 document.addEventListener('keyup', keyUp);
